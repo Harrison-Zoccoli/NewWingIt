5cbf67bdad8c9d139a223658bd62df104cee2cd8
Harrison Zoccoli <harrisonzoccoli@gmail.com>
2025-03-11 18:27:49 -0400

    Fix Azure deployment configuration

--- app.py
+++ app.py
@@ -48,4 +48,5 @@ def find_all_matches():
     return render_template('matches.html', matches=all_matches)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000) 
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port) 
